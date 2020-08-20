0a39ae820847b24afcf00b1bef80bb732afc40ee
teukurizkyn <trizkynoviandy@gmail.com>
2020-08-20 16:13:42 +0700

    update files

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
-import stats_module
+import bstats
 
 x = [84, 86, 89, 92, 82, 86, 89, 92, 80, 86, 80, 90, 87, 83]
 
 print('Total number of data   : {} data'.format(len(x)))
-print('Mean                   :', stats_module.mean(x))
-print('Median                 :', stats_module.median(x))
-print('Mode                   :', stats_module.mode(x))
-print('Variance               :', stats_module.var(x))
-print('Standar Deviation      :', stats_module.stdev(x))
+print('Mean                   :', bstats.mean(x))
+print('Median                 :', bstats.median(x))
+print('Mode                   :', bstats.mode(x))
+print('Variance               :', bstats.var(x))
+print('Standar Deviation      :', bstats.stdev(x))
 
-stats_module.describe(x)
+bstats.describe(x)
